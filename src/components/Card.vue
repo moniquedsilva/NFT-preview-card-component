930af6c08ca91b8954dfdff75390266598d97be1
Monique Silva <moniquesntsilva@gmail.com>
2021-12-16 21:55:53 -0300

    feat: add project assets

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,3 +1,21 @@
 <template>
-  <div class="hello"></div>
+  <article class="article">
+    <h1>Equilibrium #3429</h1>
+    <p>Our Equilibrium collection promotes balance and calm.</p>
+    <div>
+      <div>
+        <img src="assets/icons/icon-ethereum.svg" alt="Ethereum icon" />
+        <h3>0.041 ETH</h3>
+      </div>
+      <div>
+        <img src="assets/icons/icon-clock.svg" alt="Clock" />
+        <span>3 days left</span>
+      </div>
+    </div>
+    <img src="assets/images/image-equilibrium.jpg" alt="Equilibrium" />
+    <div>
+      <img src="assets/images/image-avatar.png" alt="Avatar" />
+      <p><span>Creation of</span> Jules Wyvern</p>
+    </div>
+  </article>
 </template>
